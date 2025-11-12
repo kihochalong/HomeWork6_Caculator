fd69f2d47ab74cdec32eaa8f9dcc8b86a64e661c
kihochalong <41330247@nfu.edu.tw>
2025-11-12 16:08:45 +0800

    Evolution Caculator Ver.2

--- widget.cpp
+++ widget.cpp
@@ -2,6 +2,7 @@
 #include <QVBoxLayout>
 #include <QGridLayout>
 #include <QPushButton>
+#include <QtMath>
 
 Widget::Widget(QWidget *parent)
     : QWidget(parent)
@@ -15,16 +16,17 @@ Widget::Widget(QWidget *parent)
     display->setMinimumHeight(70);
 
     QStringList buttons = {
+        "AC", "DEL", "(", ")",
         "7","8","9","/",
         "4","5","6","*",
         "1","2","3","-",
-        "0","AC","=","+"
+        "0",".","=","+"
     };
 
     QGridLayout *grid = new QGridLayout;
     int row = 0, col = 0;
     for(const QString &text: buttons){
-        QPushButton *btn = new QPushButton(text,this);
+        QPushButton *btn = new QPushButton(text, this);
         btn->setMinimumSize(60,40);
         grid->addWidget(btn,row,col);
         connect(btn,&QPushButton::clicked,this,[=](){ onButtonClicked(text); });
@@ -44,17 +46,18 @@ void Widget::onButtonClicked(const QString &text)
 {
     if(text=="AC"){
         display->clear();
-    }
-    else if(text=="="){
+    }else if(text=="DEL"){
+        QString t = display->text();
+        if(!t.isEmpty()) t.chop(1);
+        display->setText(t);
+    }else if(text=="="){
         QString expr = display->text();
         double result = calculate(expr);
         display->setText(QString::number(result,'g',12));
-    }
-    else{
+    }else{
         display->setText(display->text() + text);
     }
 }
-
 
 double Widget::calculate(const QString &expr)
 {
@@ -63,21 +66,44 @@ double Widget::calculate(const QString &expr)
 
     if(s.isEmpty()) return 0;
 
-    for(int i=0;i<s.size();i++){
-        if(s[i]=='+' || s[i]=='-'){
-            double left = calculate(s.left(i));
-            double right = calculate(s.mid(i+1));
-            return s[i]=='+' ? left+right : left-right;
+    int pos = s.indexOf('(');
+    while(pos!=-1){
+        int count=1;
+        int i=pos+1;
+        for(;i<s.size();i++){
+            if(s[i]=='(') count++;
+            else if(s[i]==')') count--;
+            if(count==0) break;
+        }
+        if(count!=0) return 0;
+        double val = calculate(s.mid(pos+1,i-pos-1));
+        s.replace(pos,i-pos+1,QString::number(val,'g',12));
+        pos = s.indexOf('(');
+    }
+
+
+    QStringList ops1 = {"*","/"};
+    QStringList ops2 = {"+","-"};
+
+    for(const QString &opList: {ops1.join(","), ops2.join(",")}){
+        int idx = -1;
+        for(int i=0;i<s.size();i++){
+            if(opList.contains(s[i]) && i!=0){
+                idx = i;
+                break;
+            }
+        }
+        if(idx!=-1){
+            double left = calculate(s.left(idx));
+            double right = calculate(s.mid(idx+1));
+            QChar op = s[idx];
+            if(op=='*') return left*right;
+            if(op=='/') return right!=0 ? left/right : 0;
+            if(op=='+') return left+right;
+            if(op=='-') return left-right;
         }
     }
 
-    for(int i=0;i<s.size();i++){
-        if(s[i]=='*' || s[i]=='/'){
-            double left = calculate(s.left(i));
-            double right = calculate(s.mid(i+1));
-            return s[i]=='*' ? left*right : left/right;
-        }
-    }
 
     return s.toDouble();
 }
